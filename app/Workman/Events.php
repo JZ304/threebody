fa46b98a2a588061af1d8e4513b5a497011c8f63
zhang jiaxing <13880614578@163.com>
2020-06-19 09:45:20 +0800

    长连接添加日志

--- app/Workman/Events.php
+++ app/Workman/Events.php
@@ -26,14 +26,12 @@ class Events
 
     public static function onMessage($client_id, $message)
     {
-        logger('[2][onMessage]客户端:' . $client_id . '发送信息:' . $message);
+        logger('[3][onMessage]客户端:' . $client_id . '发送信息:' . $message);
         $message = json_decode($message);
         if($message->event == 'login'){
             $tel = $message->tel;
-            logger('tel:'.$tel);
-            DB::connection()->enableQueryLog();#开启执行日志
-            UserModel::query()->where('tel',$tel)->update(['client_id' => $client_id]);
-            logger(DB::getQueryLog());   //获取查询语句、参数和执行时间
+            logger('TEL:'.$tel.';Client_id:'.$client_id);
+            UserModel::where('tel',$tel)->update(['client_id' => $client_id]);
         }else{
             // 其它业务暂不处理
             logger('其它业务暂不处理');
